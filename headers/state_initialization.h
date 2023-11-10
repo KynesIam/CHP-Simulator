ff99569d48647e03ea6a976befefcbe57d3020d3
Cian Reeves <91156701+KynesIam@users.noreply.github.com>
2023-11-10 11:22:59 -0800

    Update state_initialization.h

--- headers/state_initialization.h
+++ headers/state_initialization.h
@@ -1,3 +1,21 @@
+//
+//
+//   The routine(s) in this file are a part of the
+//                     C++Lifford
+//   suite, developed 2021, and copyrighted
+//   to the authors: Cian Reeves and Sagar Vijay
+//   at the University of California, Santa Barbara
+//
+//
+//  If you use or modify any part of this routine
+//  the header should be kept and unmodified.
+//
+//  Implements CHP unitaries with measurements and allows for determination of entanglement entropy between regions
+//  and is based on the following publication, "Improved Simulation of Stabilizer Circuits", 
+//  Scott Aaronson and Daniel Gottesman https://arxiv.org/abs/quant-ph/0406196
+//
+//
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
