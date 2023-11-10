3239a39246b1ffe0c6f0ee701bc85944746243f9
Cian Reeves <91156701+KynesIam@users.noreply.github.com>
2023-11-10 10:39:44 -0800

    Create entanglement_entropy.h

---
+++ entanglement_entropy.h
@@ -0,0 +1,9 @@
+/*
+    Implements CHP unitaries with measurements and allows for determination of entanglement entropy between regions.
+
+
+    Reference:
+        "Improved Simulation of Stabilizer Circuits"
+        Scott Aaronson and Daniel Gottesman
+        https://arxiv.org/abs/quant-ph/0406196
+*/
