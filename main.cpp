779193a1e80789a7315bfdf9f7206ecba7491651
Cian Reeves <91156701+KynesIam@users.noreply.github.com>
2023-11-10 11:22:03 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+//
+//
+//   The routine(s) in this file are a part of the
+//                     C++Lifford
+//   suite, developed 2021, and copyrighted
+//   to the authors: Cian Reeves and Sagar Vijay
+//   at the University of California, Santa Barbara
+//
+//
+//  If you use or modify any part of this routine
+//  the header should be kept and unmodified.
+//
+//  Implements CHP unitaries with measurements and allows for determination of entanglement entropy between regions
+//  and is based on the following publication, "Improved Simulation of Stabilizer Circuits", 
+//  Scott Aaronson and Daniel Gottesman https://arxiv.org/abs/quant-ph/0406196
+//
+//
+
 #include <iostream>
 #include <algorithm> 
 #include <stdio.h>
@@ -9,16 +27,6 @@
 #include "./headers/circuit.h"
 #include "./headers/entanglement_entropy.h"
 #include "./headers/state_initialization.h"
-/*
-    Implements CHP unitaries with measurements and allows for determination of entanglement entropy between regions.
-
-
-    Reference:
-        "Improved Simulation of Stabilizer Circuits"
-        Scott Aaronson and Daniel Gottesman
-        https://arxiv.org/abs/quant-ph/0406196
-*/
-
 
 int main()
 {
